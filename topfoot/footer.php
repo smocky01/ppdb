251219ecb118e44f824ea8bee15d40ebee8a1c71
smocky01 <94461586+smocky01@users.noreply.github.com>
2024-12-05 14:43:37 +0700

    Add files via upload

---
+++ topfoot/footer.php
@@ -0,0 +1,57 @@
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright Tugas Besar &copy; Web Pendaftaran SMP Slawe - 2024</span>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+ 
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Apakah anda ingin logout ?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Anda akan keluar dari Aplikasi.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+          <a class="btn btn-danger" href="<?= $base_url ?>/logout_control.php">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ 
+  <script src="../assets/jquery/jquery.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+  <script src="../assets/js/sb-admin-2.min.js"></script>
+
+  <script type="text/javascript">
+    $(function(){
+        $(".datepicker").datepicker({
+            format: 'dd-mm-yyyy',
+            autoclose: true,
+            todayHighlight: true,
+        });
+    });
+  </script>
+
+</body>
+
+</html>
